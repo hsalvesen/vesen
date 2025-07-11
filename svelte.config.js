8a1e773fb7f86baccb77f2d10d66840490e4ced5
Has <harrysalvesen@gmail.com>
2025-07-11 12:52:01 +1000

    7

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({
+    preprocess: vitePreprocess({ script: true })
+  })],
   base: '/',
   build: {
     outDir: 'dist', 
