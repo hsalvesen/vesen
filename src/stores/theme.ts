8e354206ca5afd1953ce1ef8c0b63ffd9b898afc
Has <harrysalvesen@gmail.com>
2025-07-11 10:30:48 +1000

    3

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 import themes from '../../themes.json';
 import type { Theme } from '../interfaces/theme';
 
-const defaultColorscheme: Theme = themes.find((t) => t.name === 'Slate')!;
+const defaultColorscheme: Theme = themes.find((t) => t.name === 'wombat')!;
 
 export const theme = writable<Theme>(
   JSON.parse(
