30e3d2988517d0d9bbcbb95106c5e1f8c4f3977d
Has <harrysalvesen@gmail.com>
2025-07-21 12:21:18 +1000

    Re-enabled keyboard for phone users

--- src/App.svelte
+++ src/App.svelte
@@ -24,9 +24,9 @@
 >
   <History />
 
-  <div class="flex flex-row items-baseline">
+  <div class="flex flex-col sm:flex-row mobile-prompt-stack">
     <Ps1 {isPasswordMode} />
-    <span class="mr-2"></span>
+    <span class="mr-2 hidden sm:inline"></span>
     <Input bind:isPasswordMode />
   </div>
 </main>
